362deef481f4ad714d946e093a5240efb4b7d2b2
NikosVogiatzis <nikolavg@csd.auth.gr>
2024-04-29 19:04:06 +0300

    added preprocess_data.py which is responsible for preprocessing data and removing stop words and punctuation for english

--- web_scrap/preprocess_data.py
+++ web_scrap/preprocess_data.py
@@ -3,15 +3,14 @@ from nltk.corpus import stopwords
 from nltk.tokenize import word_tokenize
 
 def remove_stopwords(text):
-    stop_words = set(stopwords.words('english'))
+    stop_words = set(stopwords.words('english')) - {'+'}
     word_tokens = word_tokenize(text)
     filtered_text = [word for word in word_tokens if word.lower() not in stop_words]
     return ' '.join(filtered_text)
 
 def preprocess_text(text):
-    # Remove digits and punctuation
-    text = re.sub(r'\d+', '', text)
-    text = re.sub(r'[^\w\s]', '', text)
+    # Remove punctuations
+    text = re.sub(r'[^\w\s+]', '', text)
     
     # Remove stop words
     text = remove_stopwords(text)
@@ -19,12 +18,11 @@ def preprocess_text(text):
     return text
 
 # Read data from file
-with open('test_data.txt', 'r') as file:
-    data = file.read()
+with open('web_scrap/commentary.txt', 'r', encoding='utf-8') as file:
+    data = file.readlines()
 
-# Preprocess data
-preprocessed_data = preprocess_text(data)
-
-# Write preprocessed data to file
-with open('updated_data.txt', 'w') as file:
-    file.write(preprocessed_data)
+# Preprocess each line of data and write it to the output file
+with open('updated_data.txt', 'w', encoding='utf-8') as file:
+    for line in data:
+        preprocessed_line = preprocess_text(line)
+        file.write(preprocessed_line.strip() + '\n')
